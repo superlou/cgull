71d2143337d55bab87c56abb8d7358e973a0a7ac
superlou <lousimons@gmail.com>
2021-09-09 09:02:07 -0400

    Added package_data to make unity accessible when not installed for development

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 from distutils.core import setup
 
 setup(name='cgull',
-      version='0.1.0',
+      version='0.1.1',
       description='Embedded code testing utility',
       author='Louis Simons',
       author_email='lousimons@gmail.com',
       packages=['cgull'],
+      package_data={'cgull': ['unity/*']},
+      include_package_data=True,
       entry_points={
         'console_scripts': ['cgull=cgull.cgull:main']
       },
