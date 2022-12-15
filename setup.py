7a80454eef433d863c9f3ef86d138d1a13482d95
superlou <lousimons@gmail.com>
2022-12-15 09:10:12 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='cgull',
-      version='0.1.1',
+      version='0.1.2',
       description='Embedded code testing utility',
       author='Louis Simons',
       author_email='lousimons@gmail.com',
